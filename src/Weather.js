fcc6ca90ffbf454337066120728175779cb87f51
Patrick <janpatrick.dev@gmail.com>
2022-03-03 15:58:08 +0800

    adjust weather left content font and alignment

--- src/Weather.js
+++ src/Weather.js
@@ -41,20 +41,26 @@ function WeatherDetails() {
     <div className="weather">
       <div className="weather__left">    
         <div className="weather__left-content">
+          <div className="weather__icon-group">
+            <img src={`http://openweathermap.org/img/wn/${weatherDetails.weather[0].icon}@2x.png`} className="weather__icon" />
+            <span className="weather__icon-description">
+              {weatherDetails.weather[0].description}
+            </span>
+          </div>
           <div className="weather__temp-group">
             <span className="weather__temp-value">
               {Math.round(weatherDetails.main.temp)}&deg;
             </span>
           </div>
           <div className="weather__city-group">
-            <span className="weather__city-name">
+            <span className="weather__city--name">
               {weatherDetails.name}
+              {weatherDetails.sys.country ? `, ${weatherDetails.sys.country}` : null}
             </span>
-            <span className="weather__city-datetime">
+            <span className="weather__city--datetime">
               {new Date(weatherDetails.dt * 1000).toUTCString()}
             </span>
           </div>
-          <img src="http://openweathermap.org/img/wn/11d@2x.png" className="weather__temp-icon" />
         </div>
       </div>
       <div className="weather__right">
@@ -68,31 +74,35 @@ function WeatherDetails() {
             <div className="weather__details--content">
               <div className="weather__details--text-group">
                 <p className="weather__details--label">Weather</p>
-                <p className="weather__details--value weather__details--weather-desc">{`${weatherDetails.weather[0].description}`}</p>
+                <p className="weather__details--value weather__details--weather-desc">{weatherDetails.weather[0].description}</p>
               </div>
               <div className="weather__details--text-group">
                 <p className="weather__details--label">Temp</p>
-                <p className="weather__details--value">{`${(Math.round(weatherDetails.main.temp * 10) / 10).toFixed(1)}`}&deg;</p>
+                <p className="weather__details--value">{(Math.round(weatherDetails.main.temp * 10) / 10).toFixed(1)}&deg;</p>
               </div>
               <div className="weather__details--text-group">
                 <p className="weather__details--label">Temp. Min</p>
-                <p className="weather__details--value">{`${(Math.round(weatherDetails.main.temp_min * 10) / 10).toFixed(1)}`}&deg;</p>
+                <p className="weather__details--value">{(Math.round(weatherDetails.main.temp_min * 10) / 10).toFixed(1)}&deg;</p>
               </div>
               <div className="weather__details--text-group">
                 <p className="weather__details--label">Temp. Max</p>
-                <p className="weather__details--value">{`${Math.round(weatherDetails.main.temp_max)}`}&deg;</p>
+                <p className="weather__details--value">{(Math.round(weatherDetails.main.temp_max * 10) / 10).toFixed(1)}&deg;</p>
               </div>
               <div className="weather__details--text-group">
                 <p className="weather__details--label">Humidity</p>
-                <p className="weather__details--value">{`${weatherDetails.main.humidity}`}%</p>
+                <p className="weather__details--value">{weatherDetails.main.humidity}%</p>
               </div>
               <div className="weather__details--text-group">
                 <p className="weather__details--label">Wind</p>
-                <p className="weather__details--value">{`${Math.round(weatherDetails.wind.speed * 3.6)}`} km/h</p>
+                <p className="weather__details--value">{`${Math.round(weatherDetails.wind.speed * 3.6)} km/h`}</p>
+              </div>
+              <div className="weather__details--text-group">
+                <p className="weather__details--label">Gusts</p>
+                <p className="weather__details--value">{weatherDetails.wind.gust ? `${Math.round(weatherDetails.wind.gust * 3.6)} km/h` : "N/A"}</p>
               </div>
               <div className="weather__details--text-group">
                 <p className="weather__details--label">Visibility</p>
-                <p className="weather__details--value">{`${weatherDetails.visibility / 1000}`} km</p>
+                <p className="weather__details--value">{`${weatherDetails.visibility / 1000} km`}</p>
               </div>
             </div>
           </div>
