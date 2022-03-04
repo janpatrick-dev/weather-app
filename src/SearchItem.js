2d967e5234333aea7d3a2c47325e125fc76d0251
Patrick <janpatrick.dev@gmail.com>
2022-03-05 01:38:53 +0800

    fix search item issue

--- src/SearchItem.js
+++ src/SearchItem.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 function SearchItem(props) {
 
-  const location = `${props.data.name}, ${props.data.state && (`${props.data.state},`)} ${props.data.country}`
-
   function handleClickItem() {
     props.onClick(props.data);
   }
   
   return  (
     <p className="searchbar__result--item" onClick={handleClickItem}>
-      {location}
+      { props.data.name }, { props.data.state && (`${props.data.state},`) } { props.data.country }
     </p>
   );
 }
